7d2b923a5d45aac4b7128857b29c5568894e528d
Ataide Bastos <ataide.bastos@gmail.com>
2017-02-08 22:39:17 -0200

    Commit inicial - Estrutura Inicial & Seeds

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table("users")->insert([
+          "name" => "Admin",
+          "email" => "admin@admin.com",
+          "password" => bcrypt("admin")
+        ]);
+
+        DB::table("users")->insert([
+          "name" => "User",
+          "email" => "user@user.com",
+          "password" => bcrypt("user")
+        ]);
+    }
+}
